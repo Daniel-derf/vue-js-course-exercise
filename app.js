5f171ee4e2907a32b7d21a75b1fb82d8152c0cb4
Daniel-derf <danielpardinho321@gmail.com>
2025-01-25 09:43:56 -0300

    fetching img

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const app = Vue.createApp({
       age: 20,
       ageIn5Years: null,
       randNum: null,
+      imageUrl: null,
+      isLoading: true,
     };
   },
   methods: {
@@ -19,10 +21,23 @@ const app = Vue.createApp({
 
       return newAge;
     },
+    async fetchImage() {
+      try {
+        const response = await fetch("https://dog.ceo/api/breeds/image/random");
+        const data = await response.json();
+        this.imageUrl = data.message;
+      } catch (error) {
+        console.error(error);
+        alert("erro ao fazer o fetch da img");
+      } finally {
+        this.isLoading = false;
+      }
+    },
   },
   created() {
     this.ageIn5Years = this.getAgeIn5Years();
     this.randNum = this.outputNum();
+    this.imageUrl = this.fetchImage();
   },
 });
 
