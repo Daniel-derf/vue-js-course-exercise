ed170515ae6d9dede59e87cb416a764d0ca31690
Daniel-derf <danielpardinho321@gmail.com>
2025-01-29 17:03:50 -0300

    vue exercise correction

--- app.js
+++ app.js
@@ -3,41 +3,16 @@ const app = Vue.createApp({
     return {
       name: "Daniel",
       age: 20,
-      ageIn5Years: null,
-      randNum: null,
-      imageUrl: null,
-      isLoading: true,
+      imageUrl: "https://images.dog.ceo/breeds/hound-afghan/n02088094_1003.jpg",
     };
   },
   methods: {
-    outputNum() {
-      const randomNumber = Math.random();
-      return randomNumber;
+    calculateAge() {
+      return this.age + 5;
     },
-    getAgeIn5Years() {
-      const newAge = this.age + 5;
-
-      console.log(this.age);
-
-      return newAge;
+    calculateRandom() {
+      return Math.random();
     },
-    async fetchImage() {
-      try {
-        const response = await fetch("https://dog.ceo/api/breeds/image/random");
-        const data = await response.json();
-        this.imageUrl = data.message;
-      } catch (error) {
-        console.error(error);
-        alert("erro ao fazer o fetch da img");
-      } finally {
-        this.isLoading = false;
-      }
-    },
-  },
-  created() {
-    this.ageIn5Years = this.getAgeIn5Years();
-    this.randNum = this.outputNum();
-    this.imageUrl = this.fetchImage();
   },
 });
 
